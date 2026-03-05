173b00ec56aaf2b4ea7caf053ee8931cee4775c1
ilya-kkk <i89213662231@gmail.com>
2026-03-05 15:31:36 +0300

    tenetnety

--- scripts/supabase/010_add_ai_comment_job_indexes.sql
+++
@@ -1,5 +0,0 @@
-create index if not exists ai_comment_jobs_user_id_idx
-    on public.ai_comment_jobs (user_id);
-
-create index if not exists ai_comment_job_posts_job_id_created_at_idx
-    on public.ai_comment_job_posts (job_id, created_at);
